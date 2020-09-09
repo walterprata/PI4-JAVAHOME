33c09d9ed0270edf50281e91e797b506e49ad462
Walter Prata <walterasprata@gmail.com>
2020-09-09 20:14:20 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/javahome/JavahomeApplication.java
@@ -0,0 +1,13 @@
+package br.com.javahome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavahomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavahomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javahome/JavahomeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javahome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavahomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
