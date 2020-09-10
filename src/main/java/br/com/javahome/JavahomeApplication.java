e5f4e69cc890edbba08045ca9b8ce2b3c20c4168
Jose-WDG <neto.silvacem@gmail.com>
2020-09-09 22:01:24 -0300

    feature:layout inicial,rotas iniciais, add algumas libs padrões.

--- src/main/java/br/com/javahome/JavahomeApplication.java
+++ src/main/java/br/com/javahome/JavahomeApplication.java
@@ -2,8 +2,9 @@ package br.com.javahome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class JavahomeApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/javahome/model/CrudRouteController.java
@@ -0,0 +1,8 @@
+package br.com.javahome.model;
+
+public interface CrudRouteController {
+    void listar();
+    void salvar();
+    void editar();
+    void deletar();
+}
