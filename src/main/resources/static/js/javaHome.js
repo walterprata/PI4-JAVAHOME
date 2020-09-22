51957f17d3ca8a17ed64f4bed6e232ff1ef846ae
Jose-WDG <neto.silvacem@gmail.com>
2020-09-21 23:06:55 -0300

    feature: ativando e desativando produtos.

--- src/main/resources/static/js/javaHome.js
+++ src/main/resources/static/js/javaHome.js
@@ -1,18 +1,96 @@
-//Hey you! Thanks for checking this pen. :) I open source it as library. https://github.com/greenwoodents/quickbeam.js
+
+
+//Apos o carregamento da pagina
 $(document).ready(function () {
     $('#money').mask("##0.00", {reverse: true});
-    $('.alert').click(function () {
-        $( ".alert" ).fadeOut();
-    });
-
 });
 
-function requestListProdutos(){
+//Ocultar alert
+$('.alert').click(function () {
+    $(".alert").fadeOut();
+});
+
+//ao clicar no botão, ele cria a tabelas com campos
+$('#btn-lista-produto').click(listarProdutos());
+
+function listarProdutos() {
+    $('#tabela tr').remove();//limpa a tabela
     $.get('/produto/listar',  // url
         function (data, textStatus, jqXHR) {  // success callback
-            for (var produtos in data.content){
-                $("#tabela").append("<tr><th scope=\"row\">"+produtos.id+"</th><td>"+produtos.nome+"</td><td>"+produtos.valor+"</td><td>"+produtos.descricao+"</td>\</tr>");
-                console.log(produtos.id);
+            if (textStatus === "success") {
+                data.content.forEach(criaTabela);
+
+                function criaTabela(produtos) {
+                    $('#tabela').append(createTabela(produtos) + estaAtivo(produtos))
+                }
             }
+        }
+    );
+}
+
+function createTabela(produtos) {
+    return "<tr><th scope=\"row\">"
+        + produtos.id +
+        "</th><td>"
+        + produtos.nome +
+        "</td><td>"
+        + produtos.descricao +
+        "</td><td>"
+        + produtos.preco +
+        "</td><td>" +
+        produtos.categoria +
+        "</td><td>" +
+        produtos.quantidade +
+        "</td><td>" +
+        "<a type=\"button\" class=\"btn btn-primary\" style='margin-right: 10px;'>Editar</a>" +
+        "<a type=\"button\" class=\"btn btn-primary\" style='margin-right: 10px;' onclick='buscarDetalhesDoProduto(" + produtos.id + ")' >Detahes</a>"
+}
+
+function estaAtivo(produto) {
+    if (produto.ativo > 0) {
+        return "<a type='button' class='btn btn-danger'  onclick='mudarStatusProduto(" + produto.id + ",\"desativa\")'>Desativar</a></td></tr>"
+    } else {
+        return "<a type='button' class='btn btn-success' onclick='mudarStatusProduto(" + produto.id + ",\"ativa\")\'>Ativar</a></td></tr>"
+    }
+}
+
+function mudarStatusProduto(id, ativa) {
+    let produtoEncontrado = buscarDetalhesDoProduto(id);
+    if (!!produtoEncontrado) {
+        if (ativa === "ativa") {
+            produtoEncontrado.ativo = 1
+        } else if (ativa === "desativa") {
+            produtoEncontrado.ativo = 0
+        } else {
+            alert("Não foi possivel definir o status");
+            return
+        }
+
+        $.ajax({
+            url: '/produto/' + id,
+            type: 'PUT',
+            success: callback,
+            data: JSON.stringify(produtoEncontrado),
+            contentType: 'application/json'
         });
+
+        function callback() {
+            alert("Produto Editado com Sucesso.");
+            listarProdutos();
+        }
+    }else{
+        alert("Não foi possivel editar produto")
+    }
+}
+
+function buscarDetalhesDoProduto(id){
+    let resultado = null;
+     $.ajax({
+        url: '/produto/' + id,
+        async:false,
+        success:function (data) {
+            resultado = data
+        }
+    })
+    return resultado
 }
