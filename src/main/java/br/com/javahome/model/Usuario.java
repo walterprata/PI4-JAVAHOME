08c36b69cdfa1e695744734daa0dacc30411380c
Walter Prata <walterasprata@gmail.com>
2020-09-26 15:44:53 -0300

    Criada entidades, usuario e cargo

---
+++ src/main/java/br/com/javahome/model/Usuario.java
@@ -0,0 +1,74 @@
+package br.com.javahome.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "senha")
+	private String senha;
+	
+	@OneToOne
+	private Cargo cargo;
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Cargo getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	
+	
+	
+	
+}
