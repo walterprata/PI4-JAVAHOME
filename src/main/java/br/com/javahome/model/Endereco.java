effc2aa53062c7607a68a6620e626cb533185a19
Walter Prata <walterasprata@gmail.com>
2020-10-25 11:20:17 -0300

    Entidade Endereco

---
+++ src/main/java/br/com/javahome/model/Endereco.java
@@ -0,0 +1,97 @@
+package br.com.javahome.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String logradouro;
+	
+	private String cidade;
+	
+	private String estado;
+	
+	private String UF;
+	
+	private String cep;
+	
+	private String complemento;
+	
+	public Endereco() {}
+
+	public Endereco(String logradouro, String cidade, String estado, String UF, String cep, String complemento) {
+		this.logradouro = logradouro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.UF = UF;
+		this.cep = cep;
+		this.complemento = complemento;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getUF() {
+		return UF;
+	}
+
+	public void setUF(String uF) {
+		UF = uF;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	
+	
+	
+}
