73f01be9ed7604ba9e16c989baae21c507d322f2
Walter Prata <walterasprata@gmail.com>
2020-11-07 16:46:07 -0300

    melhorias + repositorio de itensPedido

--- src/main/java/br/com/javahome/repository/ItensPedidoRepository.java
+++ src/main/java/br/com/javahome/repository/ItensPedidoRepository.java
@@ -3,9 +3,9 @@ package br.com.javahome.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.javahome.model.Pedido;
+import br.com.javahome.model.ItensPedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+public interface ItensPedidoRepository extends JpaRepository<ItensPedido, Integer>{
 
 }
--- src/main/java/br/com/javahome/repository/UsuarioRepository.java
+++ src/main/java/br/com/javahome/repository/UsuarioRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import br.com.javahome.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
 	@Query(value="SELECT * FROM USUARIO WHERE EMAIL = ?1", nativeQuery = true)
