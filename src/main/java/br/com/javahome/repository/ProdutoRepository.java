e125540a5a7a022c387b189929f4aff9c7e1c11c
Walter Prata <walterasprata@gmail.com>
2020-11-14 15:21:17 -0300

    Query para atualizar quantidade de produto

--- src/main/java/br/com/javahome/repository/ProdutoRepository.java
+++ src/main/java/br/com/javahome/repository/ProdutoRepository.java
@@ -1,6 +1,8 @@
 package br.com.javahome.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.javahome.model.produto.Produto;
@@ -9,4 +11,7 @@ import br.com.javahome.repository.produto.ProdutoRepositoryQuery;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>, ProdutoRepositoryQuery {
 
+	@Query(value="UPDATE produto SET quantidade = ?1 WHERE ID = ?2", nativeQuery = true)
+	public void atualizarQuantidadeDeProduto(Integer quantidade, Integer id);
+	
 }
