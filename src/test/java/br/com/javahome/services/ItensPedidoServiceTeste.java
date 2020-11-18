bcf41c1e8625a558bfd71b0d555e201c745f6efa
Walter Prata <walterasprata@gmail.com>
2020-11-17 21:13:00 -0300

    continuacao implementacao teste ItensPedido

--- src/test/java/br/com/javahome/services/ItensPedidoServiceTeste.java
+++ src/test/java/br/com/javahome/services/ItensPedidoServiceTeste.java
@@ -4,14 +4,17 @@ import br.com.javahome.model.pedido.ItensPedido;
 import br.com.javahome.model.pedido.Pedido;
 import br.com.javahome.model.produto.Produto;
 import br.com.javahome.repository.ItensPedidoRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static java.util.Optional.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 
@@ -32,7 +35,7 @@ public class ItensPedidoServiceTeste {
     }
 
     @Test
-    public void deveSalvarProdutos(){
+    public void deveAdicionarProduto(){
         //Cenario
         Produto produto = new Produto();
         Pedido pedido = new Pedido();
@@ -51,7 +54,32 @@ public class ItensPedidoServiceTeste {
         verify(repository, times(1)).save(itensPedido);
         assertEquals(listaPedido.size(), lista.size());
 
+    }
 
+    @Test
+    public void naoDeveAdicionarProduto(){
+        //Cenário
+        Pedido pedido = new Pedido();
+
+        //Quando
+        List<ItensPedido> lista = itensPedidoService.salvarProdutos(Collections.emptyList(), pedido);
+
+        //Então
+        verify(repository, never()).save(itensPedido);
+        assertTrue(lista.isEmpty());
 
     }
+    @Test
+    public void deveRertornarLista(){
+        //Cenario
+        List<ItensPedido> listaEsperada = new ArrayList<>();
+
+        //Quando
+        List<ItensPedido> lista = itensPedidoService.listarPedidos();
+        when(repository.findAll()).thenReturn(listaEsperada);
+
+        //Então
+        assertEquals(listaEsperada, lista);
+    }
+    
 }
